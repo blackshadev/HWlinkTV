7693d72219b0803d4871f0eeebc9b19dab20dabb
Vincent Hagen <vincent.hagen@student.uva.nl>
2014-04-12 12:36:45 +0200

    Works

--- StatRed/week1/biometrie.py
+++ StatRed/week1/biometrie.py
@@ -3,7 +3,11 @@ import scipy.stats as stats
 import numpy as np
 import csv
 import math
+import matplotlib.pyplot as plt
 
+
+def getRows(items, mask):
+    return np.array([items[i] for i in range(len(mask)) if mask[i]])
 
 def readCsv(fname, skip):
     a = []
@@ -19,39 +23,46 @@ class NBClassifier:
     """
     In this case Males will be identified by 0, females by 1
     """
-    def __init__(self, arr, t):
+    __dict__ = ["means", "stds", "arr", "t", "labels"]
+    def __init__(self, arr, t, labels):
         # we assume that in arr, we predict the first row, ad te rest is data
         self.arr = arr
         # t is the apprioti change of female divided by that of a male
         self.t = t
-    def learn(self):
-
+        self.means = []
+        self.stds = []
+        self.labels = labels
+    def learn(self, idx=None):
         self.normals = []
 
         # all indices used in the learning process
-        idx = range(0, len(self.arr))
+        if idx == None:
+            idx = range(0, len(self.arr))
+
         items = np.array([self.arr[i] for i in idx])
 
         # Mask for all males
-        mask = items[:,0] < 1
+        mask = np.ma.array(items[:,0] < 1)
+        subs = getRows(items, mask)
 
         # calculate one patch of normals (for males)
-        means = np.mean(items[mask][:,1:], axis=0)
-        stds  = np.std(items[mask][:,1:], axis=0)
+        self.means.append(np.mean(subs[:,1:], axis=0))
+        self.stds.append(np.std(subs[:,1:], axis=0))
 
-        self.normals.append([stats.norm(loc=means[i], scale=stds[i]) \
-              for i in range(0, len(means))])
+        self.normals.append([stats.norm(loc=self.means[0][i], \
+            scale=self.stds[0][i]) for i in range(0, len(self.means[0]))])
 
         # inverse mask. Mark for the ladies
         mask = [not i for i in mask]
+        subs = getRows(items, mask)
 
         # Calc mean and stdev for females
-        means = np.mean(items[mask][:,1:], axis=0)
-        stds  = np.std(items[mask][:,1:], axis=0)
+        self.means.append(np.mean(subs[:,1:], axis=0))
+        self.stds.append(np.std(subs[:,1:], axis=0))
 
-        self.normals.append([stats.norm(loc=means[i], scale=stds[i]) \
-              for i in range(0, len(means))])
-
+        self.normals.append([stats.norm(loc=self.means[1][i], \
+            scale=self.stds[1][i]) for i in range(0, len(self.means[1]))])
+    # False = female, True = male
     def test(self, dat):
         chance = [];
 
@@ -64,24 +75,44 @@ class NBClassifier:
         # log chance to be a male, using the sum of logpdf for females
         chance.append(sum([self.normals[1][i].logpdf(dat[i]) for i in range(len(dat))]))
 
-        print chance
-
         t = chance[0] - chance[1]
 
         return t > math.log(self.t);
+    """
+    
+    """
+    def confusionMatrix(self):
+        matrix = np.zeros((2,2))
+        for i in range(0, len(self.arr)):
+            # Learn set by index
+            idx = range(0, len(self.arr))
+            del idx[i]
+            self.learn(idx)
 
+            actual = self.arr[i];
+            res = 0 if self.test(actual[1:]) else 1
+            matrix[actual[0], res] += 1
+        return matrix
 
+    def plot(self):
+        for i in range(0, len(self.normals[0])):
+            loc = min([self.means[0][i], self.normals[1][i]])
+            std = max([5 * self.stds[0][i], 5 * self.stds[1][i]])
+            x = np.linspace(loc - std, loc + std, 100 * std)
+            plt.figure()
+            plt.title(self.labels[i+1])
+            plt.plot(x, self.normals[0][i].pdf(x), label="Mannen")
+            plt.plot(x, self.normals[1][i].pdf(x), label="Vrouwen")
+            plt.legend()
+            plt.show()
 
 
 def main():
     arr = readCsv("biometrie2014.csv", 5)
-    nb = NBClassifier(arr, 1.)
-    nb.learn()
-    print nb.test([55, 170, 42]) # ik
-    print nb.test([98, 189, 44]) # man
-    print nb.test([63, 173, 41]) # vrouw
-
-
+    nb = NBClassifier(arr, 1., ["Geslacht", "Gewicht", "Lengte", "Schoenmaat"])
+    # nb.learn()
+    # nb.plot()
+    print nb.confusionMatrix()
 
 if __name__ == '__main__':
     main()
