bbe40a6e1685207d8ee1d5d13c6bb7e2db1dda67
Tessa Klunder <tessa.klunder@student.uva.nl>
2014-05-03 11:16:51 +0200

    comments part 1

--- StatRed/week4/ex21.py
+++ StatRed/week4/ex21.py
@@ -4,16 +4,16 @@
 " Date   : 29-4-2014
 "
 " Plots multivariant normal distributed values for a given mu and sigma
-" It plots these values as a n x n matrix of plots where the index within 
-"  the matrix (i, j) matches the dimention which are plotted against each other 
+" It plots these values as a nxn matrix of plots where the index within 
+" the matrix (i, j) matches the dimension which are plotted against each other 
 "
 """
 
 import numpy as np
 import matplotlib.pyplot as plt
 
-""" Returns n samples of a normal distributed random variables with given
- 	mean, and covariance """
+# Returns n samples of normal distributed random variables with given
+# mean and covariance
 def multivariate(mean, cov, n):
 	d, U = np.linalg.eig(cov)
 	L = np.diagflat(d)
@@ -24,7 +24,7 @@ def multivariate(mean, cov, n):
 if __name__ == '__main__':
 	samples = 1000
 	mean = [[0],[1],[2],[-1]]
-	cov = [[1,0,-3,2],[0,1,2,-3],[-2,0,0,1],[9,3,-4,2]]
+	cov  = [[1,0,-3,2],[0,1,2,-3],[-2,0,0,1],[9,3,-4,2]]
 
 	Y = multivariate(mean, cov, samples)
 
@@ -37,7 +37,7 @@ if __name__ == '__main__':
 			# No axes for clarity in the plot
 			ax.xaxis.set_visible(False)
 			ax.yaxis.set_visible(False)
-
+			#skip the diagonal
 			if i == j: continue
 			
 			ax.scatter(Y[i], Y[j])
